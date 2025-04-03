dc121a5dc20575d8f51d4d19ae8c0ce87993e63e
Sachin ST <itssachinst@gmail.com>
2025-04-03 23:15:38 +0530

    content type 20250403

--- app/utils/s3_utils.py
+++ app/utils/s3_utils.py
@@ -25,7 +25,7 @@ def upload_to_s3(file_path: str, file_name: str) -> str:
     if not S3_BUCKET:
         raise ValueError("S3_BUCKET is not defined. Check your environment variables.")
     print(f"https://{S3_BUCKET}.s3.eu-north-1.amazonaws.com/{file_name}")
-    s3_client.upload_file(file_path, S3_BUCKET, file_name)
+    s3_client.upload_file(file_path, S3_BUCKET, file_name, ExtraArgs={"ContentType": "video/mp4"})
     
     return f"https://{S3_BUCKET}.s3.eu-north-1.amazonaws.com/{file_name}"
 
